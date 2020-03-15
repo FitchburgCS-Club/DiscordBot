27c178c905eb3ed2b2dba3b83ba4902a95349de7
zja203 <crazycreeper17@gmail.com>
2020-03-14 20:09:49 -0400

    Added !featurerequest

--- src/main/java/com/fsucsc/discordbot/CommandListener.java
+++ src/main/java/com/fsucsc/discordbot/CommandListener.java
@@ -4,6 +4,9 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.GenericMessageEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CommandListener extends ListenerAdapter {
 	//NOTE(Michael):we use onGenericMessage to allow for command testing in PMs.
 	public void onGenericMessage (GenericMessageEvent e) {
@@ -14,6 +17,17 @@ public class CommandListener extends ListenerAdapter {
 				msg.getChannel().sendMessage("Pong!").queue ();
 		}
 
-		//TODO(Michael): Add more commands here...
+		if (rawMsg.startsWith("!featurerequest")) {
+			try {
+				FileWriter fw = new FileWriter(DisConfig.outDir + "FeatureRequests.txt", true);
+				String message = msg.getContentStripped();
+				message = message.replace("\n", " ");
+				fw.write(message.substring(16) + "\n");
+				fw.close();
+				msg.getChannel().sendMessage("Submission Received!").queue();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 }
---
+++ src/main/java/com/fsucsc/discordbot/DisConfig.java
@@ -0,0 +1,11 @@
+package com.fsucsc.discordbot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class DisConfig {
+	static String token = null;
+	static String whitelistedUser = "zja203#5285";
+	static String outDir = ""; // Make sure to include the trailing "/"
+	static List<String> blackListedUsers = new ArrayList<>();
+}
