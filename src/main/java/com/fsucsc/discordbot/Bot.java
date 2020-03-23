1127a45ed7516549b3d5c142475422894f8dc28b
Michael Roskuski <mroskuski@gmail.com>
2020-03-23 00:22:40 -0400

    Change try into try-with-resources
    Applicable try blocks have been changed into try-with-resources blocks, which automatically call close on closeable resources like Scanner and FileWriter.

--- src/main/java/com/fsucsc/discordbot/Bot.java
+++ src/main/java/com/fsucsc/discordbot/Bot.java
@@ -11,8 +11,7 @@ import java.util.Scanner;
 
 public class Bot {
 	public static void main (String[] args) {
-		try {
-			Scanner scan = new Scanner(new File("config"));
+		try (Scanner scan = new Scanner(new File("config"));) {
 			DisConfig.token = scan.nextLine();
 			if (scan.hasNextLine()) {
 				DisConfig.whitelistedUser = scan.nextLine();
@@ -24,10 +23,14 @@ public class Bot {
 			System.out.println("Are you sure it exists and contains the token for the discord bot?");
 			//NOTE(Michael): when working on the project, the 'config' file should be placed at the root of the repo
 			System.exit(-1);
+		} catch (Exception ex) {
+			System.out.println("Abnormal error occured");
+			ex.printStackTrace();
+			System.exit(-1);
 		}
 
 		try {
-			 new JDABuilder(AccountType.BOT).setToken(DisConfig.token).addEventListeners(new CommandListener()).build();
+			new JDABuilder(AccountType.BOT).setToken(DisConfig.token).addEventListeners(new CommandListener()).build();
 		} catch (LoginException | IllegalArgumentException ex) {
 			System.out.println("Failed to Log in");
 			System.exit(-2);
--- src/main/java/com/fsucsc/discordbot/CommandListener.java
+++ src/main/java/com/fsucsc/discordbot/CommandListener.java
@@ -62,12 +62,10 @@ public class CommandListener extends ListenerAdapter {
 		}
 
 		if (rawMsg.startsWith("!featurerequest")) {
-			try {
-				FileWriter fw = new FileWriter(DisConfig.outDir + "FeatureRequests.txt", true);
+			try (FileWriter fw = new FileWriter(DisConfig.outDir + "FeatureRequests.txt", true);) {
 				String message = msg.getContentStripped();
 				message = message.replace("\n", " ");
 				fw.write(message.substring(16) + "\n");
-				fw.close();
 				msg.getChannel().sendMessage("Submission Received!").queue();
 			} catch (IOException ex) {
 				ex.printStackTrace();
